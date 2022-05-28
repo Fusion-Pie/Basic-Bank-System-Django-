e1fdb8ffe0750ef2568a1679ae0b7f5b02e91b29
Fusion-Pie <piyushhatewar@gmail.com>
2022-05-28 22:06:59 +0530

    Second commit

--- Pie_bank/settings.py
+++ Pie_bank/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a4-drw6wdg6grlbmo_16k)chav=#!m1_a^n%y)abfu7iae*yzw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # APPEND_SLASH = False
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- Pie_bank/urls.py
+++ Pie_bank/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# Added for hosting
+from django.views.static import serve
+from django.conf.urls import url
+
+from django.conf import settings
+
 # Added to change the title, header which is first Django Administration
 admin.site.site_header = "Pie Bank of India Admin Panel"
 admin.site.site_title = "PBI Admin Portal"
@@ -9,4 +15,7 @@ admin.site.index_title = "Welcome to Pie Bank Of India"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('pie_bank_app.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
